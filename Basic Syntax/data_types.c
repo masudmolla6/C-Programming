43c7ef5fe0420f9cc656311cdaf271de0cd94a7f
masudmolla6 <masudmolla2937@gmail.com>
2025-11-14 20:28:09 +0600

    different types of data

---
+++ Basic Syntax/data_types.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    // integer type Variale.
+    int age=10;
+    printf("My age is %d\n", age);
+
+    // floating type Variale.
+    float gpa=4.56;
+    printf("My Result is %f\n", gpa);
+
+    // Double type Variale.
+    double result=2.92;
+    printf("My Result is %.2lf\n", result);
+
+    // Character type Variale.
+    char initial='A';
+    printf("My Result is %c\n", initial);
+
+    // String type Variale.
+     char myName[]="Masud";
+    printf("My Result is %s\n", myName);
+
+    return 0;
+}
